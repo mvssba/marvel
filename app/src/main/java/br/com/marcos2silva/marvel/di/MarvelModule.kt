f68de594c309e09eacdc2e28555e217d7655cb4b
Marcos Silva <mvssba@gmail.com>
2021-09-13 11:26:00 -0300

    extract tokens for properties

--- app/src/main/java/br/com/marcos2silva/marvel/di/MarvelModule.kt
+++ app/src/main/java/br/com/marcos2silva/marvel/di/MarvelModule.kt
@@ -1,20 +1,22 @@
 package br.com.marcos2silva.marvel.di
 
 import androidx.paging.PagingSource
-import br.com.marcos2silva.marvel.data.repository.MarvelRepository
-import br.com.marcos2silva.marvel.data.repository.MarvelRepositoryImpl
 import br.com.marcos2silva.marvel.characters.datasource.CharactersPagingSource
 import br.com.marcos2silva.marvel.characters.presentation.CharactersViewModel
-import br.com.marcos2silva.marvel.data.datasource.remote.MarvelRemoteDataSourceImpl
-import br.com.marcos2silva.marvel.data.response.CharacterResponse
-import br.com.marcos2silva.marvel.data.datasource.remote.MarvelRemoteDataSource
+import br.com.marcos2silva.marvel.data.api.MarvelService
 import br.com.marcos2silva.marvel.data.datasource.local.FavoriteLocalDataSource
+import br.com.marcos2silva.marvel.data.datasource.remote.MarvelRemoteDataSource
+import br.com.marcos2silva.marvel.data.datasource.remote.MarvelRemoteDataSourceImpl
+import br.com.marcos2silva.marvel.data.local.FavoriteDataBase
+import br.com.marcos2silva.marvel.data.repository.MarvelRepository
+import br.com.marcos2silva.marvel.data.repository.MarvelRepositoryImpl
+import br.com.marcos2silva.marvel.data.response.CharacterResponse
 import br.com.marcos2silva.marvel.details.presentation.CharacterDetailViewModel
 import br.com.marcos2silva.marvel.favorites.CharactersFavoritesViewModel
-import br.com.marcos2silva.marvel.data.local.FavoriteDataBase
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 object MarvelModule {
 
@@ -35,17 +37,30 @@ object MarvelModule {
     }
 
     private val data = module {
-        factory<PagingSource<Int, CharacterResponse>> { CharactersPagingSource(service = get(), "") }
+        factory<PagingSource<Int, CharacterResponse>> {
+            CharactersPagingSource(
+                service = get(),
+                ""
+            )
+        }
         factory<MarvelRemoteDataSource> { MarvelRemoteDataSourceImpl(service = get()) }
         factory<FavoriteLocalDataSource> { get<FavoriteDataBase>().favoriteDao() }
     }
+
+    private val service = module {
+        factory { providerMarvelService(get()) }
+    }
+
+    private fun providerMarvelService(retrofit: Retrofit) =
+        retrofit.create(MarvelService::class.java)
 
     fun load() {
         loadKoinModules(
             listOf(
                 presentation,
                 domain,
-                data
+                data,
+                service
             )
         )
     }
--- network/src/main/java/br/com/marcos2silva/network/StringExtensions.kt
+++ network/src/main/java/br/com/marcos2silva/network/StringExtensions.kt
@@ -1,11 +1,11 @@
-package br.com.marcos2silva.marvel.extensions
+package br.com.marcos2silva.network
 
 import java.math.BigInteger
 import java.security.MessageDigest
 
 private const val ALGORITHM = "MD5"
 
-fun String.md5(): String =
+internal fun String.md5(): String =
     BigInteger(
         1, MessageDigest.getInstance(ALGORITHM)
             .digest(toByteArray())
